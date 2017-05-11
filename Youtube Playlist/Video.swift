227465e5ec301f35e459fd9a1bd299fd43e730e2
MRisser1 <student@MRisser1.local>
2017-05-11 11:38:46 -0500

    Started over from scratch.

---
+++ Youtube Playlist/Video.swift
@@ -0,0 +1,13 @@
+//
+//  Video.swift
+//  Youtube Playlist
+//
+//  Created by MRisser1 on 5/11/17.
+//  Copyright © 2017 MRisser1. All rights reserved.
+//
+
+import UIKit
+
+class Video: NSObject {
+
+}
